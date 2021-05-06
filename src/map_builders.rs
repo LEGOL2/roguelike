4157389afbc58d3b2cd965b858818c6299de22c3
Sebastian Luzynski <sebastian.luzynski@gmail.com>
2021-05-06 10:59:19 +0200

    Add BSP map generation

--- src/map_builders.rs
+++ src/map_builders.rs
@@ -1,6 +1,8 @@
 use crate::{Map, Position};
+use bracket_lib::prelude::RandomNumberGenerator;
 use specs::prelude::World;
 
+mod bsp_map;
 pub mod common;
 mod simple_map;
 
@@ -14,5 +16,10 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    Box::new(simple_map::SimpleMapBuilder::new(new_depth))
+    let mut rng = RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 2);
+    match builder {
+        1 => Box::new(bsp_map::BspMapBuilder::new(new_depth)),
+        _ => Box::new(simple_map::SimpleMapBuilder::new(new_depth))
+    }    
 }
